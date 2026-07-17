4883813873fe0b897bfb1ee13a08f6d1745272e9
dishant1123 <dishantshah1417@gmail.com>
2026-07-17 16:27:49 +0530

    ....

---
+++ basic/17july.py
@@ -0,0 +1,57 @@
+# convert  : 
+
+"""
+a =10 
+print("a value is  : ",a)
+print("a value convert in to the  float type  : ",float(a))
+
+
+b=12.78 
+print("b value is  : ",b)
+print("b value convert in to the  int type  : ",int(b))
+print("b value  bool value is : ",bool(b))
+
+float_to_string =str(b)
+print("float_to_string value is : ",float_to_string)
+print(type(float_to_string))  # "12.78"
+"""
+
+# user input  int: 
+
+"""
+a=int(input("enter the a value : "))
+b=int(input("enter the b value : "))
+
+print("a=",a)
+print("b=",b)
+print("a+b =",a+b)
+
+# note : In interger data type  you can't store float  value. it give value error.
+"""
+"""
+# user input float :
+a=float(input("enter the a value : "))
+b=float(input("enter the b value : "))
+
+print("a=",a)
+print("b=",b)
+print("a+b =",a+b)
+# note: In float data type  you can store integer  value. 
+"""
+
+# user input string  : 
+
+"""a=str(input("enter the name : "))
+b=input("enter the sur-name : ")
+
+print(a,end=" ")
+print(b)  # gaurav sharma
+"""
+
+# task  :1 ask user to enter the  2 number  and  concate  them . 
+"""
+input  a= 10 
+input  b= 20
+output  :1020 
+"""
+
