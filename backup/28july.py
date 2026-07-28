a6d10b7bf8d72208640cfbcf603e22f4022229f6
dishant1123 <dishantshah1417@gmail.com>
2026-07-28 15:50:04 +0530

    ....

---
+++ backup/28july.py
@@ -0,0 +1,49 @@
+"""
+python  : 
+1. high programming languges 
+2. object oriented programming
+3. interpreted programming languages 
+
+use : 
+1.backend : ===> framework django ,flask ,odoo 
+2.data analysis :   ===> insights  ===> numpy ,pandas ,matplotlib ,seaborn
+3.data cleaning : ===> 
+4.AI -ML : machine learning ,artificial intelligence
+5.GUI : graphical user interface
+6.game development :
+7. hacking 
+extention : .py 
+
+run  : VS code ,jpyter ,annaconda ,google colab
+
+project : spootify , you tube , PUBG ,
+"""
+
+# print("hello world")
+"""print("my name is ved")
+print("my name is ram")
+
+print("shah")
+"""
+# comment: 
+"""
+1.single line comment  : # 
+2.multi line comment   :  ''' '''  or """ """ 
+"""
+# escap sequence character :
+
+"""
+1.\n : new line
+2.\t : tab
+3.\b : backspace
+4. end =  : end of line   ===> remove white space
+"""
+
+# print("ram\n")
+# print("shah")
+# print("ramt\t\t shah")
+
+# print("ram\b\bshah")
+
+print("dishant",end="\b\b")
+print("shah")
