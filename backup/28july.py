02cb08aea00e3f7262eb7018c426b639df6eb435
dishant1123 <dishantshah1417@gmail.com>
2026-07-30 14:32:24 +0530

    ....

--- backup/28july.py
+++ backup/28july.py
@@ -48,4 +48,92 @@ print("shah")
 """print("dishant",end="\b\b")
 print("shah")
 """
+# variable declaration  rules : 
+"""
+1. any variable you can't start with digit , special character 
+    exception : _  ====> valid for variable 
 
+2. you can end variable digit 
+
+"""
+
+# data type  : 
+"""
+1.int  : pos  or neg 
+2.float  : decimal 
+3.char/string : a to z ,digits ,
+4.bool  : True,False 
+5.complex  : 1. real part  , 2.immiginary part  
+"""
+
+"""a=9023452345345345634563453456456456   # a variable , 90 static value 
+print(a)
+print(type(a))
+print("a=",a)
+
+b=23556.45456564565676767
+print(b)
+print(type(b))
+print("b=",b)
+
+c= "abnm"
+print(c)
+print(type(c))
+
+d=True
+print(d)
+print(type(d))
+
+e = 23 + 8j 
+print(e)
+print(type(e))
+f= 45+20j 
+print(f+e)
+"""
+
+# convert data type 
+
+"""a=90 
+print(a)
+print("a is  int : ",type(a))
+print("a value  convert in to float : ",float(a))
+print("a value  convert in to complex : ",complex(a))
+print("a value  convert in to string : ",str(a))
+print("a value  convert in to bool : ",bool(a))
+"""
+
+# user input  int  : 
+
+"""
+a=int(input("enter the  a  number :"))
+b=int(input("enter the  b  number :"))
+
+print("a=",a)
+print("b=",b) # note : int data type  not store float value.  
+"""
+
+# user input  float  :
+
+"""a= float(input("enter the  a  number :"))
+b= float(input("enter the  a  number :"))
+
+print("a=",a)
+print("b=",b) # note : float data type  store int value.  
+"""
+
+# string user input :
+
+"""a=str(input("enter the  a  name :"))
+b= input("enter the  b  sur-name :")
+
+print(a)
+print(b)
+print(type(a))
+"""
+
+# ask user to enter the two int data type number and  concate them.
+"""
+input a =10 
+input b =20
+output 1020
+"""
