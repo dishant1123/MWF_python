750913f4bf7d6f06f86b4e64d7916b98d4f5f6bf
dishant1123 <dishantshah1417@gmail.com>
2026-08-05 16:30:46 +0530

    ....

---
+++ string/5aug.py
@@ -0,0 +1,43 @@
+# string  : immutable  sequence of characters , you can't change the value of a string
+
+"""
+s1="my name is hetvi."
+print(s1)
+print(type(s1))
+"""
+# slicing : 
+"""
+s1="my name is hetvi."
+#   01234           
+print(s1[2])
+print(s1[10])
+print(s1[1:5])
+print(s1[:11])
+print(s1[1 :])
+print(s1[-1])
+print(s1[1 : 12 :2])
+print(s1[ :  :3])
+print(s1[ :  :1])
+print(s1[ -5: -2 ])
+print(s1[ :  : -1 ])
+"""
+
+# task :1  using slicing  only 
+"""
+input : dishant dipakkumar shah
+output : d.d.shah
+"""
+# task :2 using slicing only 
+"""
+ask user to  enter two string  and  interchange the  first three characters of the first string with the first three characters of the second string. 
+
+input 1 : color   mital  ----> mit --->patal
+input 2 : full    patil  ----> pat --->mitil 
+
+output 1 : fulor 
+output 2 : coll
+"""
+
+
+
+
